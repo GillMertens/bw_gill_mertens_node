6ce910752eef6418cd1c0e36c008f64dc87f577e
Gill Mertens <gill.mertens@student.ehb.be>
2024-08-18 21:04:49 +0200

    Feature: setup express server and implement posts and comments routes

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const postRoutes = require('./routes/posts');
+const commentRoutes = require('./routes/comments');
+
+const app = express();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+pool.connect()
+  .then(() => console.log('PostgreSQL connected...'))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.json());
+
+app.use('/posts', postRoutes);
+app.use('/comments', commentRoutes);
+
+app.listen(3000, () => console.log('Server started on port 3000'));
