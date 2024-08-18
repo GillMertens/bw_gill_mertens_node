88cbeae4c51d53baa2d1624aab7d49dc9516f547
Gill Mertens <gill.mertens@student.ehb.be>
2024-08-18 20:54:32 +0200

    Chore: add migrations for posts and comments

---
+++ migrations/1723941420911_create-posts-table.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('posts', {
+        id: 'id',
+        title: { type: 'varchar(255)', notNull: true },
+        content: { type: 'text', notNull: true },
+    });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('posts');
+};
---
+++ migrations/1723941435467_create-comments-table.js
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+    pgm.createTable('comments', {
+        id: 'id',
+        post_id: { type: 'integer', notNull: true, references: 'posts' },
+        content: { type: 'text', notNull: true },
+    });
+}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+    pgm.dropTable('comments');
+};
