b6b190624b113503ffa09a70fdb30d2564a4f020
Gill Mertens <gill.mertens@student.ehb.be>
2024-08-19 20:48:59 +0200

    Fix: authorization bugs

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,11 +1,17 @@
 module.exports = (getModelById) => {
   return async (req, res, next) => {
-    const { id } = req.params;
+    const id = Number(req.params.id);
     const model = await getModelById(id);
     if (!model) {
       return res.status(404).json({ message: 'Not found' });
     }
-    if (req.user.role === 'admin' || model.user_id === req.user.id) {
+    let modelId;
+    if (model.hasOwnProperty('user_id')) {
+      modelId = model.user_id;
+    } else {
+      modelId = model.id;
+    }
+    if (req.user.role === 'admin' || modelId === req.user.id) {
       return next();
     }
     return res.status(403).json({ message: 'Forbidden' });
